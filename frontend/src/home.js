9e36d032655f955586567d976b2c3182cd41eefb
NGU123-coder <diwakarzxcvbnm@gmail.com>
2026-06-07 14:24:40 +0530

    UI Rebranding: Remove CodeBasics references and assets, replace with Potato Disease Classifier branding

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -9,7 +9,7 @@ import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Paper, CardActionArea, CardMedia, Grid, TableContainer, Table, TableBody, TableHead, TableRow, TableCell, Button, CircularProgress } from "@material-ui/core";
-import cblogo from "./cblogo.PNG";
+import leaf from "./leaf.jpg";
 import image from "./bg.png";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { common } from '@material-ui/core/colors';
@@ -216,7 +216,7 @@ export const ImageUpload = () => {
             Disease Classifier : Potato Disease Classification
           </Typography>
           <div className={classes.grow} />
-          <Avatar src={cblogo}></Avatar>
+          <Avatar src={leaf}></Avatar>
         </Toolbar>
       </AppBar>
       <Container maxWidth={false} className={classes.mainContainer} disableGutters={true}>
